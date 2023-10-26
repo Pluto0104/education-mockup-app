da939150c72024d22ba41885a780f4f946d6bef3
DESKTOP-3JG190E\Pluto <fed.dev.pluto@gmail.com>
2023-10-26 14:25:56 -0500

    Define react navigator type for typescript

---
+++ src/types/routes.ts
@@ -0,0 +1,14 @@
+export type RootStackParamList = {
+  Welcome: undefined;
+  SignUp: undefined;
+  SignIn: undefined;
+  Grade: undefined;
+  Province: undefined;
+  Dashboard: undefined;
+};
+
+export type DashboardTabParamList = {
+  Explore: undefined;
+  Stream: undefined;
+  ClassWork: undefined;
+};
